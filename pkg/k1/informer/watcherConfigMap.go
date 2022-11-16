41278474dbc75ac700b5f12148d0e446cc3acf27
6za <53096417+6za@users.noreply.github.com>
2022-11-16 17:00:07 +0000

    revert meta object
    Signed-off-by: 6za <53096417+6za@users.noreply.github.com>

--- pkg/k1/informer/watcherConfigMap.go
+++ pkg/k1/informer/watcherConfigMap.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/tools/cache"
 )
@@ -21,7 +20,7 @@ func WatchConfigMap(conditions []BasicConfigurationCondition, matchConditions ch
 		AddFunc: func(obj interface{}) {
 			// "k8s.io/apimachinery/pkg/apis/meta/v1" provides an Object
 			// interface that allows us to get metadata easily
-			mObj := obj.(*metav1.ObjectMeta)
+			mObj := obj.(*corev1.ConfigMap)
 			logger.Debug(fmt.Sprintf("New Pod updated:", mObj.GetName(), mObj.GetNamespace()))
 			checkMatchBasicConfigurationCondition(&BasicConfiguration{Namespace: mObj.GetNamespace(), Name: mObj.GetName()}, conditions, matchConditions)
 
@@ -29,14 +28,14 @@ func WatchConfigMap(conditions []BasicConfigurationCondition, matchConditions ch
 		UpdateFunc: func(old, new interface{}) {
 			// "k8s.io/apimachinery/pkg/apis/meta/v1" provides an Object
 			// interface that allows us to get metadata easily
-			newObj := new.(*metav1.ObjectMeta)
+			newObj := new.(*corev1.ConfigMap)
 			logger.Debug(fmt.Sprintf("Pod updated:", newObj.GetName(), newObj.GetNamespace()))
 			checkMatchBasicConfigurationCondition(&BasicConfiguration{Namespace: newObj.GetNamespace(), Name: newObj.GetName()}, conditions, matchConditions)
 		},
 		DeleteFunc: func(obj interface{}) {
 			// "k8s.io/apimachinery/pkg/apis/meta/v1" provides an Object
 			// interface that allows us to get metadata easily
-			mObj := obj.(*metav1.ObjectMeta)
+			mObj := obj.(*corev1.ConfigMap)
 			logger.Debug(fmt.Sprintf("New Pod deleted from Store: %s", mObj.GetName()))
 		},
 	})
